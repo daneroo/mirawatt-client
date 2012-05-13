53b27cabecab0b574b651b3c7b79eacfbda58a56
Daniel Lauzon <daniel.lauzon@gmail.com>
2012-05-13 15:22:27 -0400

    remove zing and dnode reference

--- client.js
+++ client.js
@@ -4,36 +4,15 @@ var jsonrpc = require('./lib/jsonrpc-client');
 var request = require('request');
 
 var endpoint='http://0.0.0.0:3000/jsonrpc';
-//var endpoint='http://mirawatt.cloudfoundry.com/jsonrpc';
+if (process.argv.length>2){
+    endpoint = endpoint=process.argv[2];
+}
+console.log('endpoint',endpoint,process.argv);
 var client = jsonrpc(endpoint);
 
 
-var method='zing';
-var params=[44]; //{ n: param }, 
-client.call(method,params,function(err, result) {
-  if (err){
-    console.log('jsonrpc.zing('+params+') Error: ',err);
-  } else {
-    console.log('jsonrpc.zing('+params+') = ' + result);
-  }
-});
-
-// dnode part
-if (0) dnode.connect(7070, function (remote, conn) {
-  //console.log(conn);
-  var param=42;
-  remote.zing(param, function (err,result) {
-    if (err){
-      console.log('remote.zing('+param+') Error: ',err);
-    } else {
-      console.log('remote.zing('+param+') = ' + result);
-    }
-    conn.end();
-  });
-});
-
 function fetch(cb){
-  console.log('fetching')
+  console.log(new Date().toISOString(),'fetching')
   request.get({uri:"http://cantor.imetrical.com/iMetrical/feedsJSON.php", json : true},function(error,response,body){
     cb(error,body);
   });
