cf4e0dd86786e027056ec5594a9adfa15c6e9b1e
Daniel Lauzon <daniel.lauzon@gmail.com>
2012-05-21 17:09:40 -0400

    move code to lib/mirawatt.js, work on dnode lifecycle

--- client.js
+++ client.js
@@ -1,7 +1,7 @@
 // Dependancies for json-rpc and dnode clients 
 var dnode = require('dnode');
 var jsonrpc = require('./lib/jsonrpc-client');
-var request = require('request');
+var mirawatt = require('./lib/mirawatt');
 
 var endpoint='http://0.0.0.0:8080/jsonrpc';
 if (process.argv.length>2){
@@ -9,35 +9,6 @@ if (process.argv.length>2){
 }
 console.log('endpoint',endpoint,process.argv);
 var client = jsonrpc(endpoint);
-
-
-function fetch(cb){
-  console.log(new Date().toISOString(),'fetching','for',endpoint);
-  request.get({uri:"http://cantor.imetrical.com/iMetrical/feedsJSON.php", json : true},function(error,response,body){
-    cb(error,body);
-  });
-}
-
-function byN(feeds,N){
-  // clone
-  feeds = JSON.parse(JSON.stringify(feeds));
-  feeds.feeds.forEach(function(feed,scopeId){
-    feed.sensorId=[];//"s1","s2"
-    for (i=1;i<=N;i++){
-      feed.sensorId.push('s'+i);
-    }
-    feed.obs.forEach(function(o){ // mult by [1,2,..,N]/(N*(N+1)/2)
-      var v=o.v[0]/(N*(N+1)/2);
-      o.v=[];
-      var i;
-      for (i=1;i<=N;i++){
-        var vi=Math.round(v*i*10)/10;
-        o.v.push(vi);
-      }
-    });
-  });
-  return feeds;
-}
 
 function push(userId,feeds){
   client.call('set',[userId,feeds],function(err,result){
@@ -68,13 +39,13 @@ function getCheck(userId){
 // push, then get
 function fetchAndPush(){
     var userId='daniel';
-    var feeds={stamp:new Date(),value:Math.random()};
-    fetch(function(err,feeds){
+    console.log(new Date().toISOString(),'fetching','for',endpoint);    
+    mirawatt.iMetricalFetch(function(err,feeds){
         push(userId,feeds);
         // if (1) getCheck(userId);
         var Ns=[2,8];//[2,4,8];
         Ns.forEach(function(N){
-          push(userId+'By'+N,byN(feeds,N));
+          push(userId+'By'+N,mirawatt.byN(feeds,N));
           // if (1) getCheck(userId+'By'+N);        
         });
     });
--- dclient.js
+++ dclient.js
@@ -1,10 +1,13 @@
 var io = require('socket.io-client');
-var sock = io.connect('http://localhost:8080');
+var sock = io.connect('http://localhost:8080',{
+  reconnect:false,
+  //'max reconnection attempts':3
+});
 // var sock = io.connect('http://mw-spec.cloudfoundry.com:80');
 // var sock = io.connect('http://mw-spec.jit.su:80');
 var dnode = require('dnode');
-
 var Stream = require('stream');
+
 var stream = new Stream;
 stream.writable = true;
 stream.readable = true;
@@ -21,43 +24,80 @@ sock.on('message', function (msg) {
     stream.emit('data', msg);
 });
 
+sock.on('connecting', function () {
+  console.log('  --sock connecting',new Date().toISOString());
+});
+
 sock.on('connect', function () {
     console.log('  --sock connect',new Date().toISOString());
     stream.emit('connect');
 });
 
-var remoteServer = null;
-dnode(function (client, conn) {
-    ['connect','ready','remote','end','error','refused','drop','reconnect'].forEach(function(ev){
-        conn.on(ev,function(){
-            console.log('  --dnode.conn',conn.id,ev,new Date().toISOString());
-        });
-    });
-}).connect(stream, {reconnect:5000}, function (remote,conn) {
-    console.log('dnode connect');
-    remoteServer=remote;
-    setTimeout(function(){
-        remoteServer=null;
-        conn.end();
-    },300000000);
+sock.on('connect_failed', function () {
+  console.log('  --sock connect_failed',new Date().toISOString());
 });
 
-setInterval(doZing,3000);
+sock.on('disconnect', function () {
+  console.log('  --sock disconnect',new Date().toISOString());
+  stream.emit('end');
+});
 
-function doZing(cb){
-    if (remoteServer){
-        console.log('doZing',new Date().toISOString());
-        [10,/* 0, 1, 10, 1234*/].forEach(function(zing){
-            remoteServer.zing(zing,function (err,zong) {
-                if (err){
-                    console.dir(err);
-                } else {
-                    console.log('zing-zong: ' + zong);
-                }
-                if (cb) cb();
-            });
-        });
-    } else {
-        console.log('skipping doZing');
-    }
+sock.on('reconnecting', function () {
+  console.log('  --sock reconnecting',new Date().toISOString());
+});
+
+sock.on('reconnect', function () {
+  console.log('  --sock reconnect',new Date().toISOString());
+});
+
+var dn = dnode(function (client, conn) {
+  console.log('*****new client/conn',conn.id);
+  this.type='sensorhub';
+  ['connect','ready','remote','end','error','refused','drop','reconnect'].forEach(function(ev){
+    conn.on(ev,function(){
+      console.log('  --dnode.conn',conn.id,ev,new Date().toISOString());
+    });
+  });
+  var intervalId;
+  conn.on('ready',function(){
+    theConn=conn;
+    console.log('dnode ready',conn.id);
+    intervalId=setInterval(doZing,3000,client)
+  });
+  conn.on('end',function(){
+    console.log('dnode end',conn.id);
+    clearInterval(intervalId);    
+  });
+}).connect(stream /*, {reconnect:5000}*/);
+
+setInterval(function(){
+  var p = sock.socket;
+  console.log('sock monitor',p.connected,p.connecting,p.reconnecting/*,p.transport,p.options*/);
+  if (!(p.connected || p.connecting || p.reconnecting)){
+    console.log('reconnect...');
+    sock.socket.connect();
+  }
+  // console.log('sock monitor',sock);
+},5000);
+// setInterval(doZing,3000);
+
+function doZing(remote,cb){
+  if (!sock.socket.connected) {
+    console.log('doZing - should not happen!')
+  }
+  if (sock.socket.connected && remote){
+    console.log('doZing',new Date().toISOString());
+    [10,/* 0, 1, 10, 1234*/].forEach(function(zing){
+      remote.zing(zing,function (err,zong) {
+        if (err){
+          console.dir(err);
+        } else {
+          console.log('zing-zong: ' + zong);
+        }
+        if (cb) cb();
+      });
+    });
+  } else {
+    console.log('skipping doZing');
+  }
 }
