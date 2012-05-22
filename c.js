0d642919d69f9c7b575fa6287224528a03be2434
Daniel Lauzon <daniel.lauzon@gmail.com>
2012-05-22 19:36:35 -0400

    standalone test for dnode over http-websocket

--- c.js
+++ c.js
@@ -1,14 +1,55 @@
+var io = require('socket.io-client');
+var Stream = require('stream');
 var dnode = require('dnode');
-function doit(){
-  dnode.connect(7070, {reconnect:5000},function (remote, conn) {
 
+
+function doit(){
+  var sock = io.connect('http://localhost:8080',{
+    reconnect:false,
+    // 'try multiple transports':false,
+    //'max reconnection attempts':3
+    // transports:['websocket', 'flashsocket', 'htmlfile', 'xhr-multipart', 'xhr-polling', 'jsonp-polling']
+  });
+  
+  var stream = new Stream;
+  stream.writable = true;
+  stream.readable = true;
+  stream.write = function (buf) {
+      sock.emit('message', String(buf));
+  };
+  stream.destroy = stream.end = function () {
+      console.log('  --stream destroy-end');
+      // stream.emit('end');
+      sock.disconnect();
+  };
+  
+  ['connecting','connect','connect_failed','disconnect','reconnecting','reconnect','error','end'].forEach(function(ev){
+    sock.on(ev,function(){
+      console.log('  --sock',ev,new Date().toISOString());
+    });
+  });
+    
+  sock.on('message', function (msg) {
+      stream.emit('data', msg);
+  });
+
+  sock.on('connect', function () {
+      stream.emit('connect');
+  });
+
+  sock.on('disconnect', function () {
+    stream.emit('end');
+  });
+  
+  dnode(function (remote, conn) {
+    console.log('*****new remote/conn',conn.id);
+    this.type='sensorhub';
     ['connect','ready','remote','end','error','refused','drop','reconnect'].forEach(function(ev){
       conn.on(ev,function(){
         console.log('  --dnode.conn',conn.id,ev,new Date().toISOString());
       });
     });
-
-    var count=20;
+    var count=5;
     var intervalId;
     conn.on('ready',function(){
       console.log('dnode ready',conn.id);
@@ -16,10 +57,9 @@ function doit(){
     });
     conn.on('end',function(){
       console.log('dnode end',conn.id);
-      clearInterval(intervalId);    
+      clearInterval(intervalId);
     });
-
-
+    
     function doZing(){
       if (!--count){
         conn.end();
@@ -29,9 +69,9 @@ function doit(){
         console.log('n=' + n,'count',count,'conn',conn.id);
       });
     }
+    
+  }).connect(stream /*, {reconnect:5000}*/);;
 
-  });  
 }
 
 doit();
-setTimeout(doit,6000);
--- s.js
+++ s.js
@@ -1,11 +1,20 @@
+// Config section
+var port = (process.env.VMC_APP_PORT || 8080);
+var host = (process.env.VCAP_APP_HOST || '0.0.0.0'|| 'localhost');
+
+var express = require('express');
+var server = express.createServer();
+
 var dnode = require('dnode');
 
-var server = dnode(function (remote, conn) {
+dnode(function (remote, conn) {
     this.zing = function (n, cb) { cb(n * 100) };
     ['connect','ready','remote','end','error','refused','drop','reconnect'].forEach(function(ev){
       conn.on(ev,function(){
         console.log('  --dnode.conn',conn.id,ev,new Date().toISOString());
       });
     });
-});
-server.listen(7070);
+}).listen(server);
+
+server.listen(port, host);
+console.log('http://'+host+':'+port+'/');
